2fd1b4ba63e60e2a01da637832eacb50272a8d54
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-02-23 17:36:38 +0000

    inital commit

---
+++ scripts/install-rpms.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo '##########################################################################'
+echo '##### About to run install-rpms.sh script ##################'
+echo '##########################################################################'
+
+yum install -y epel-release 
+yum install -y vim 
+yum install -y bash-completion 
+yum install -y mtr
+yum install -y traceroute 
