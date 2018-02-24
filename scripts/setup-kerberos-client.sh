1c2e2ac51a1f94186e0470622660e4685e0e0bd3
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-02-24 13:16:48 +0000

    wip

--- scripts/setup-kerberos-client.sh
+++ scripts/setup-kerberos-client.sh
@@ -9,6 +9,28 @@ echo '##########################################################################
 
 yum install -y krb5-workstation pam_krb5
 
+
+cp /etc/krb5.conf /etc/krb5.conf-orig
+# the following replaces a whole line based on a partial match
+# https://stackoverflow.com/questions/11245144/replace-whole-line-containing-a-string-using-sed
+sed -i '/default_realm/c\ default_realm = CODINGBEE.NET' /etc/krb5.conf
+
+
+sed -i 's/# EXAMPLE.COM/  CODINGBEE.NET/g' /etc/krb5.conf
+
+sed -i 's/#  kdc = kerberos.example.com/   kdc = kdc.codingbee.net/g' /etc/krb5.conf
+sed -i 's/#  admin_server = kerberos.example.com/   admin_server = kdc.codingbee.net/g' /etc/krb5.conf
+sed -i 's/# }/}/g' /etc/krb5.conf     # this should edit the line that's right after the admin_server line. 
+
+sed -i 's/# .example.com = EXAMPLE.COM/ .codingbee.net = CODINGBEE.NET/g' /etc/krb5.conf
+sed -i 's/# example.com = EXAMPLE.COM/ codingbee.net = CODINGBEE.NET/g' /etc/krb5.conf
+
+cp /var/kerberos/krb5kdc/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl-orig
+
+sed -i 's/EXAMPLE.COM/CODINGBEE.NET/g' /var/kerberos/krb5kdc/kadm5.acl
+
+
+
 kadmin <<EOF
 MySecretRootPassword
 addprinc -randkey host/kerberos-client.codingbee.net 
